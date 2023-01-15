75cd268c699ba16730f490614d2d73b682a15a49
József Ádám Punk <72190642+dzsozef13@users.noreply.github.com>
2023-01-16 00:59:44 +0100

    cleaning

--- Space Invasion/Core/GameViewController.swift
+++ Space Invasion/Core/GameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
